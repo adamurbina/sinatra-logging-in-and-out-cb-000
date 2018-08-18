a64ac2aeb1b2217508d8fc6808ea683f078bf5b3
Adam Urbina <adamurbina@gmail.com>
2018-08-18 21:48:52 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,7 @@ class Helpers
         User.find_by_id(sessions[:user_id])
     end
 
-    def self.is_logged_in?(session)
+    def self.is_logged_in?(sessions)
         !!User.find_by_id(sessions[:user_id])
     end
 end
