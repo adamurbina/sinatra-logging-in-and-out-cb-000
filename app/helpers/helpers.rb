507f620326b940f4db205864136f6c3ceba9b892
Adam Urbina <adamurbina@gmail.com>
2018-08-18 22:07:31 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-    def self.current_user(sessions)
-        User.find_by_id(sessions[:user_id])
+    def self.current_user(session)
+        User.find_by_id(session[:user_id])
     end
 
-    def self.is_logged_in?(sessions)
-        !!User.find_by_id(sessions[:user_id])
+    def self.is_logged_in?(session)
+        !!User.find_by_id(session[:user_id])
     end
 end
