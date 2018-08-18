810e15aa86dde9fc3cb9e4c10a8330903835f4f8
Adam Urbina <adamurbina@gmail.com>
2018-08-18 21:13:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-      @user = User.find_by(email: params[:email])
+      @user = User.find_by(username: params[:username])
+      if @user
+          
+      end
   end
 
   get '/account' do
